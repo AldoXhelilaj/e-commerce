13fed9188b6f71d81e54752a24949996398ec605
aldo <aldoxhelili69@hotmail.com>
2020-12-11 17:17:35 +0100

    Updates

---
+++ precache-manifest.f0bd5f1531bc09e5de7020eaab964a08.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b5260e55df49f601c84b66fb7ad36361",
+    "url": "/e-commerce/index.html"
+  },
+  {
+    "revision": "ccb4c98b2e22fe1cb7d9",
+    "url": "/e-commerce/static/css/main.1fa27324.chunk.css"
+  },
+  {
+    "revision": "288ef3e85ac82cc0f5cc",
+    "url": "/e-commerce/static/js/2.7015344f.chunk.js"
+  },
+  {
+    "revision": "f9e7e68e90600aabdd7d1f759413461d",
+    "url": "/e-commerce/static/js/2.7015344f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ccb4c98b2e22fe1cb7d9",
+    "url": "/e-commerce/static/js/main.12d75e03.chunk.js"
+  },
+  {
+    "revision": "f0bd369c1ff9feedfaf5",
+    "url": "/e-commerce/static/js/runtime-main.96a20351.js"
+  },
+  {
+    "revision": "30f0707118fb960665491424c115ac44",
+    "url": "/e-commerce/static/media/shopping-bag.30f07071.svg"
+  }
+]);
